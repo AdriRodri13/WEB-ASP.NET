7af4d665e1a7776fb6fe0fa03c5dd3c5105a3d71
AdriRodri13 <152196347+AdriRodri13@users.noreply.github.com>
2025-01-08 20:03:31 +0100

    Tarifas

---
+++ WEB ASP.NET/Data/TarifasContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WEB_ASP.NET.Models;
+
+namespace WEB_ASP.NET.Data
+{
+    public class TarifasContext : DbContext
+    {
+        public TarifasContext(DbContextOptions<TarifasContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Tarifas> Tarifas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configuración de la tabla
+            modelBuilder.Entity<Tarifas>().ToTable("Tarifas");
+
+            // Configura el ValueConverter para el campo Atributos
+            modelBuilder.Entity<Tarifas>()
+                .Property(t => t.Atributos)
+                .HasConversion(
+                    v => string.Join(",", v),  // Convierte la lista a un string (separado por comas)
+                    v => v.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries) // Convierte el string en una lista (separado por comas)
+                );
+        }
+    }
+}
